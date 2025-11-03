4dc8d90c6642db8291de2e3d544d635b04a25fe0
Aaron Hamilton <aaronleon28@gmail.com>
2025-11-03 11:35:37 +0000

    Images for question 3

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -110,60 +110,60 @@ const questions =[
         "<p>Raja Kumari has co-written some great songs <br>'Centuries' by Fall Out Boy<br>'Change Your Life' by Iggy Azalea<br>'Goddess' by Krewella, NERVO <br>'Renegade' on Arcane League of Legends </p>"
     ),
     new Question(
-        "",
+        "/images/quiz/youtube_logo.webp",
         "Which One?",
         "Which YouTube Channel lost major credibility due to none stop mistakes, and overworking to produce content on a strict tight schedule.",
 
-        "",
+        "/images/quiz/jerryrigeverything_portrait.webp",
         "JerryRigEverything",
 
-        "",
+        "/images/quiz/mrwhosetheboss_portrait.webp",
         "Mrwhosetheboss",
 
-        "",
+        "/images/quiz/ufd_tech_portrait.webp",
         "UFD Tech",
 
-        "",
+        "/images/quiz/linus_tech_tips_portrait.webp",
         "Linus Tech Tips",
 
         4,
         "<p>Linus Tech Tips (LTT), known for its consistent and high-volume upload schedule, faced significant criticism over declining quality assurance and factual errors in mid-2023. The situation escalated following a public report highlighting these issues, leading the company to issue a formal apology and implement a week-long production break (known as 'Restructure'). This pause was used to re-evaluate internal processes, prioritise accuracy and quality over speed, and publicly commit to stricter quality control moving forward. </p>"
     ),
     new Question(
-        "",
+        "/images/quiz/",
         "Which One?",
         "Which device’s battery got punctured when JerryRigEverything did his bend test.",
 
-        "",
+        "/images/quiz/",
         "Nokia 3310",
 
-        "",
+        "/images/quiz/",
         "Google Pixel 10 Pro Fold",
 
-        "",
+        "/images/quiz/",
         "Samsung Galaxy Z Fold7",
 
-        "",
+        "/images/quiz/",
         "Apple iPhone Air",
 
         2,
         "<p>Jerry did his usual test to check the durability of the current device compared to the last generation. When he bent the phone in its unfolded mode. The device gave up where the antenna line lied. While fidgeting with the device, the battery gets punctured and then the device starts to smoke and the battery reacted badly and heated up. He got very lucky the device didn’t catch on flames.</p>"
     ),
     new Question(
-        "",
+        "/images/quiz/",
         "Which One?",
         "Which company was doing so well with their console and then royally ruined it with their next generation.",
 
-        "",
+        "/images/quiz/",
         "Microsoft",
 
-        "",
+        "/images/quiz/",
         "Sony",
 
-        "",
+        "/images/quiz/",
         "Google",
 
-        "",
+        "/images/quiz/",
         "Nintendo",
 
         2,
